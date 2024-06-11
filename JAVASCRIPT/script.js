e0261cb3f9f075bb4d44a2cf5a65c28d3e9f6ce6
Stefan Tošić <153987296+TosicS@users.noreply.github.com>
2024-06-11 22:02:35 +0200

    Add files via upload

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,79 @@
+
+function insertTextImage(id)
+{
+    var text = text = document.getElementById('tekstic');;
+    var image = image = document.getElementById('slikica');
+
+    if( id == 0)
+        {
+            text.innerText = "Kineski zid predstavlja najveću građevinu na svetu i svakako najzanimljiviju turističku atrakciju u Kini. Njegova izgradnja trajala je dugo, a kao najznačajniji period izdvaja se period od 220. do 206. godine pre nove ere u doba Ćin Ši Huanga koji je započeo tj. nastavio izgradnju da bi zaštitio zemlju od upada varvarskih plemena. Duž celog zida poređane su stražarnice, a po vrhu se pruža staza široka 4.5 metra. Gradnja zida je okončana u 17. veku i od tada neprestano privlači pažnju svojom monumentalnošću.";
+            image.src = "../Image/kineski-zid-1.png"
+        }  
+
+    if( id == 1)
+        {
+            text.innerText = "Petra se nalazi u Jordanu, čuvena po svojoj arhitekturi, i sistemu za navodnjavanje. Poznata i pod imenom ‘’Crveni grad’’ zbog boje stena u kojima je isklesana. Osnovana je 312. godine pre nove ere, kao glavni grad arapskog nabatejskog carstva. Leži na pola puta od zaliva Akaba ka Mrtvom moru, na nadmorskoj visini od 800 pa do 1.350 metara u području Edoma. Zapadnom svetu je predstavljena 1812. godine, nakon što je otkrio švajcarski istraživač Johan Ludvig Burhard. Trenutno je najposećenija turisticka atrakcija u Jordanu.";
+            image.src = "../Image/petra-2.png"
+        }  
+
+    if( id == 2)
+        {
+            text.innerText = "Hristos Spasitelj (Cristo Redentor) je najveća postojeća art deko skulptura, a 5. najveća statua Isusa na svetu. Simbol je hrišćanstva, predstavlja Isusa Hrista raširenih ruku, okrenutog ka gradu. Prava je kulturna ikona i obeležje kako Rio de Žaneira, tako i samog Brazila. Nalazi se na planini Korkovadu, na 709 metara nadmorske visine, visoka je 38 metara, od čega je samo postolje visoko 8 metara. Skulptura je izgrađena je od ojačanog betona, a prekrivena mekim kamenom. Građena je od 1922. godine, a otkrivena i predstavljena javnosti 12. oktobra 1931.godine.";
+            image.src = "../Image/isus-rio-3.png"
+        }  
+    if( id == 3)
+        {
+            text.innerText = "Taž Mahal je sagradio Šah Džahan u čast svoje žene Ardžumand Bano Begum, koja je bila poznaija kao Mumtaz Mahal. Ova impozantna građevina nalazi se na obali reke Džamuna u indijskom gradu Agra. Tadž Mahal se smatra vrhunskim ostvarenjem mogulske umetnosti i opisan je kao ’’dragulj islamske umetnosti u Indiji’’. Jedan je od svetski najpoznatijih remek dela i simbol je bogate istorije Indije. Tadž Mahal godišnje poseti preko 3 miliona posetilaca.";
+            image.src = "../Image/tadz-mahal-4.png"
+        }  
+    if( id == 4)
+        {
+            text.innerText = "Maču pikču (Machu Picchu) smešten na najvišem delu istočnih Anda, na skoro 2.350 metara iznad nivoa mora iznad doline Urubamba u Peruu. Grad je takođe poznat i kao ‘’Izgubljeni grad Inka’’. Izgrađen je oko 1450. godine, kada je imperija Inka bila na vrhuncu. Napušten je samo 100 godina kasnije, kao rezultat španskih osvajačkih pohoda. Tek 1911. godine otkriva ga amerikanac Hajram Bingam";
+            image.src = "../Image/macu-pikcu-5.png"
+        }  
+    if( id == 5)
+        {
+            text.innerText = "Aja Sofija ili Sveta Sofija (Αγία Σοφία na grčkom ili Ayasofya na turskom), remek delo arhitekture, nekadašnja pravoslavna patrijaršijska bazilika, kasnije carska džamija, a sada muzej u Istanbulu. Aja Sofija je trenutno drugi najposećenije muzej u Turskoj koji godišnje poseti 3.3 miliona posetilaca. Od vremena izgradnje pa do 1453. služila je kao pravoslavna crkva (sem u periodu od 1204. do 1261. godine, kada je pretvorena u katoličku crkvu). Nakon toga postala je džamija, što je i ostala sve do 1931. godine. Zanimljivo je da je do izgradnje Plave džamje (1616. godine) bila glavna džamija u Istanbulu. Muzej je postala 01. februara 1935 godine.";
+            image.src = "../Image/aja-sofia-6.png"
+        }  
+    if( id == 6)
+        {
+            text.innerText = "Čičen Ica (Chichén Itzá) što na jeziku Maja znači ” Na izvoru Ica (naroda)” je danas veliko arheološko nalazište iz doba civilizacije Maja i nalazi se na poluostrvu Jukatan u Meksiku. U centru Čičen Ice Maje su sagradile veliku stepenastu piramidu’’ koja je visoka 24 metra i ima 9 velikih terasa za koje se smatra da predstavljaju devet oblasti podzemnog sveta prema mitologiji Maja. Veruje se da su ljudi bacani sa vrha ove piramide kao žrtve za svoju dobru sreću, a oni koji su preživeli smatrani su vidovnjacima.";
+            image.src = "../Image/cicenica-7.png"
+        }  
+    if( id == 7)
+        {
+            text.innerText = "Koloseum (Colosseum), izvorno nazvan “Flavijev teatar” je eliptični amfiteatar koji se nalazi u centru Rima u Italiji, korišćen kao mesto za javne spektakle. Najveća je građevina tog tipa na svetu i procenjuje se da je mogao da primi izmdju 50 i 80 hiljada ljudi. Ima trospratnu fasadu, prizemlje koje je urađeno u dorskom stilu, prvi sprat u jonskom, a treći u korintskom. Ovo je jedna od najvećih arhitektonskih dela ikada sagrađenih u istoriji Rima.";
+            image.src = "../Image/koloseum-8.png"
+        }  
+    if( id == 8)
+        {
+            text.innerText = "Gradnja Torre pendente di Pisa ili jednostavnije Torre di Pisa je počela 14. avgusta 1173 godine, a završena je 1360 godine. Ovaj toranj je, kao što mu i ime kaže, poznat po tome što ima nagib na svojoj desnoj strani i to čak 5.5%. Visina tornja od zemlje do niže strane je 55.86 metara, a 56.7m od zemlje do više strane. Toranj je zatvoren za posetioce 1990. godine radi sanacije, a ponovo je otvoren u decembru 2001 godine. Njegov nagib je tada vraćen za 44 centimetara, tj u stanje iz 1838. godine.";
+            image.src = "../Image/krivi-toranj-u-pizi-9.png"
+        }  
+    if( id == 9)
+        {
+            text.innerText = "Čuveni kompleks Rimskih kupatila (Roman Baths) od istorijskog je značaja za region Somerset. Nalazi se u gradiću Bat (Bath), nedaleko od Bristola. Mesto je nastalo još u vreme kada su antički Rimljani došli u Britaniju i otkrili izvore tople vode u ovom mestu. U 6. veku nove ere je potpuno uništeno, a onda je kroz vreme rekonstruisano, te je današnji izgled dobilo u 19. veku. Kupatila su velika turistička atrakcija koju poseti više od milion posetilaca godišnje. U emisiji ’’ Sedam prirodnih čuda’’ emitovanoj na BBC-ju 2005 godine, predstavljena su kao jedno od čuda zapadnog sveta.  Posetioci mogu posetiti kupatila i muzej, ali ulazak u vodu nije dozvoljen.";
+            image.src = "../Image/rimska-kupala.png"
+        }  
+
+}
+
+var dugme = document.getElementById('dugme1');
+
+dugme.addEventListener('click', function() {
+    window.location.href = '../HTML/index.html'; 
+});
+
+var dugme = document.getElementById('dugme2');
+
+dugme.addEventListener('click', function() {
+    window.location.href = '../HTML/Oautoru.html'; 
+});
+
+var dugme = document.getElementById('dugme3');
+
+dugme.addEventListener('click', function() {
+    window.location.href = '../HTML/Uputstvo.html'; 
+});
+
